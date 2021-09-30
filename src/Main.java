e8b152687a443c57f91e38e0704f172974a43e66
Leiza Girfatova <leiza-rad@yandex.ru>
2021-09-30 09:24:40 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int tiketPrice = 50;
+        int count = 4;
+        int cardPrice = 50;
+        int totalSum = tiketPrice * count + cardPrice;
+        System.out.println(totalSum);
+    }
+}
+
+
+
